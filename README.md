01bf3555d41cd8cd17806ff238083d2adc881306
Raymond Cheung <rkfcheung@gmail.com>
2021-01-18 22:40:44 +0800

    Add comparison

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Simple Least Recently Used Cache
 
 * [Cache Replacement Policies: Least Recently Used (LRU)](https://en.wikipedia.org/wiki/Cache_replacement_policies#LRU)
+
+## SimpleCache vs MemoryCache
+
+ Test Count | SimpleCache (ms) | MemoryCache (ms)
+------------|------------------|-----------------
+2,048 | 17 | 74
+8,192 | 46 | 87
+32,768 | 191 | 289
+131,072 | 760 | 1,127
--- SimpleLruCache/Program.cs
+++ SimpleLruCache/Program.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace SimpleLruCache
 {
     class Program
     {
-        static void Main(string[] args)
+        class SimpleKey
         {
-            Console.WriteLine("Hello World!");
+            Guid guid = Guid.NewGuid();
+            DateTime dateTime = DateTime.Now;
+
+            public override string ToString()
+            {
+                return $"SimpleKey({guid}, {dateTime})";
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("## SimpleCache vs MemoryCache");
+            Console.WriteLine();
+            Console.WriteLine(" Test Count | SimpleCache (ms) | MemoryCache (ms)");
+            Console.WriteLine("------------|------------------|-----------------");
+            int maxTestCount = 2 << 16;
+            for (int testCount = 2 << 10; testCount <= maxTestCount; testCount <<= 2)
+            {
+                int capacity = 128;
+                var cacheOptions = new MemoryCacheOptions { SizeLimit = capacity };
+                var entryOptions = new MemoryCacheEntryOptions { Size = 1 };
+                var client = new HttpClient();
+                var requestUrl = "https://httpbin.org/cache";
+                var response = await client.GetAsync(requestUrl);
+                SimpleKey key = null;
+                var stopWatch = new Stopwatch();
+
+                stopWatch.Start();
+                var simpleCache = new SimpleCache(capacity);
+                for (int i = 0; i < testCount; i++)
+                {
+                    simpleCache.Get(key);
+                    key = new SimpleKey();
+                    simpleCache.Set(key, response);
+                    Debug.Assert(simpleCache.Count <= capacity);
+                }
+                stopWatch.Stop();
+                var timeForSimpleCache = stopWatch.ElapsedMilliseconds;
+
+                stopWatch.Reset();
+                stopWatch.Start();
+                var memoryCache = new MemoryCache(cacheOptions);
+                for (int i = 0; i < testCount; i++)
+                {
+                    memoryCache.Get(key);
+                    key = new SimpleKey();
+                    memoryCache.Set(key, response, entryOptions);
+                    Debug.Assert(memoryCache.Count <= capacity);
+                }
+                stopWatch.Stop();
+                var timeForMemoryCache = stopWatch.ElapsedMilliseconds;
+
+                Console.WriteLine($"{testCount} | {timeForSimpleCache} | {timeForMemoryCache}");
+            }
         }
     }
 }
